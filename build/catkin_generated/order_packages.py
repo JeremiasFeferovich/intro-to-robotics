76056a82c13dd5bb638621e602124807f1fe1173
JeremiasFeferovich <jeremias@feferovich.com.ar>
2024-09-19 17:40:29 +0100

    create package

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jerefefe/ros_workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jerefefe/r1_wiki_ws/devel;/opt/ros/noetic'.split(';') if '/home/jerefefe/r1_wiki_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/miniproject1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerefefe/ros_workspace/src/miniproject1
+# Build directory: /home/jerefefe/ros_workspace/build/miniproject1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

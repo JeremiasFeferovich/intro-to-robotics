d2b1f86f7a7b9ce9417944736e77844038889540
JeremiasFeferovich <jeremias@feferovich.com.ar>
2024-09-24 09:14:40 +0100

    gitignore

--- build/miniproject1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jerefefe/ros_workspace/src/miniproject1
-# Build directory: /home/jerefefe/ros_workspace/build/miniproject1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
